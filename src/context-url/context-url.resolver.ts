7cb1ad5fb8cd8d79e8f46a5cee6a2a84b907d1a9
chibek <sergibelemarketing@gmail.com>
2022-05-29 12:04:58 +0200

    Add update delete and updateClick

--- src/context-url/context-url.resolver.ts
+++ src/context-url/context-url.resolver.ts
@@ -2,6 +2,8 @@ import {Args, Mutation, Query, Resolver} from '@nestjs/graphql';
 import {Url} from "./entities/url.entity";
 import {ContextUrlService} from "./context-url.service";
 import {CreateUrlInput} from "./dto/create-url.input";
+import {UpdateUrlInput} from "./dto/update-url.input";
+
 
 @Resolver(() => Url)
 export class ContextUrlResolver {
@@ -22,4 +24,27 @@ export class ContextUrlResolver {
     create(@Args('createUrl') createUrl: CreateUrlInput): Promise<Url> {
         return this.urlService.create(createUrl);
     }
+
+    @Mutation(() => Url)
+    update(@Args('updateUrl') updateUrl: UpdateUrlInput): Promise<Url> {
+        return this.urlService.update(updateUrl);
+    }
+
+    @Mutation(() => String)
+    async delete(@Args('id', {type: () => String}) id: string): Promise<String> {
+        return await  this.urlService.delete(id);
+    }
+
+    @Mutation(() => Url)
+    async updateClick(@Args('id', {type: () => String}) id: string): Promise<Url> {
+        return await this.urlService.updateClick(id);
+        // await this.pubSub.publish(SUBSCRIPTION_EVENTS.urlClicked, {
+        //     urlClicked: url
+        // });
+    }
+
+    // @Subscription(() => Url)
+    // getClicks() {
+    //     return this.pubSub.asyncIterator(SUBSCRIPTION_EVENTS.urlClicked);
+    // }
 }
--- src/context-url/context-url.service.ts
+++ src/context-url/context-url.service.ts
@@ -1,8 +1,10 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, Param} from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
 import {Url} from "./entities/url.entity";
 import {FindOptionsWhere, Repository} from "typeorm";
 import {CreateUrlInput} from "./dto/create-url.input";
+import {UpdateUrlInput} from "./dto/update-url.input";
+
 
 @Injectable()
 export class ContextUrlService {
@@ -27,5 +29,25 @@ export class ContextUrlService {
             shortUrl: dynamicShortUrl
         });
         return this.urlRepository.save(newUrl);
+    }
+
+    async update(updateUrl: UpdateUrlInput): Promise<Url> {
+        await this.urlRepository.update(updateUrl.id, updateUrl);
+        return await this.findOne({id: updateUrl.id});
+    }
+
+    async delete(@Param('id') id: string): Promise<String> {
+        try {
+            await this.urlRepository.delete(id);
+        } catch (e) {
+            console.log(e)
+        }
+        return id;
+    }
+
+    async updateClick(@Param('id') id: string): Promise<Url> {
+        const urlUpdated: Url = await this.findOne({id: id});
+        urlUpdated.clicks++;
+        return this.urlRepository.save(urlUpdated);
     }
 }
